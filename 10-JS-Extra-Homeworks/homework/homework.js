324a53fd02d0992d651ce1169dbca56d5f70a773
Agagus <carol@LAPTOP-3KV2U6LS>
2022-07-29 19:05:15 -0300

    completed homework 10

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -1,5 +1,7 @@
 // No cambies los nombres de las funciones.
 
+const { convertToRecursiveGlob } = require("@11ty/eleventy/src/TemplatePath");
+
 function deObjetoAmatriz(objeto){
   // Escribe una función que convierta un objeto en una matriz, donde cada elemento representa 
   // un par clave-valor en forma de matriz.
@@ -10,6 +12,13 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
+
+  const array = []
+  // Object.keys(objeto)
+  for (prop in objeto){
+    array.push([prop, objeto[prop]])
+  }
+  return array
 }
 
 
@@ -18,14 +27,33 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
+  let obj = {}
+  for (let i=0; i<string.length; i++) {
+    if (string[i] in obj){
+      obj[string[i]]++ ;
+    }
+      else obj[string[i]] = 1 ;
+  }
+  return obj ;
 }
 
 
 function capToFront(s) {
   //Realiza una función que reciba como parámetro un string y mueva todas las letras mayúsculas
-  //al principio de la palabra.
+  //al principio de la .
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+  let mayus = ''
+  let minus = ''
+  for (i=0; i<s.length ; i++){
+    if (s[i] === s[i].toUpperCase()){
+      mayus = mayus + s[i]
+    }
+    else {
+      minus = minus + s[i]
+    }
+  }
+  return mayus + minus
 }
 
 
@@ -35,6 +63,11 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+  let array = str.split(' ')
+  for (indice in array){
+    array[indice] = array[indice].split("").reverse().join("")
+  }
+  return array.join(' ')
 } 
 
 
@@ -43,6 +76,13 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
+  var nstr = numero.toString()
+  for (let i=0 ; i+1 < nstr.length/2 ; i++) {
+    if (nstr[i] !== nstr[nstr.length-i-1]){
+      return "No es capicua"
+    }
+  }
+  return "Es capicua"
 }
 
 
@@ -50,6 +90,7 @@ function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
+  return cadena.replaceAll('a','').replaceAll('b','').replaceAll('c','')
 }
 
 
@@ -57,15 +98,32 @@ function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+  
+  return arr.sort(function (a, b) {
+    if (a.length > b.length) {
+      return 1;
+    }
+    if (a.length < b.length) {
+      return -1;
+    }
+    return 0;
+  })
 }
 
 
 function buscoInterseccion(arreglo1, arreglo2){
-  //Existen dos arrays, cada uno con 5 números. A partir de ello, escribir una función que permita 
-  //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,3,4] = [3,4].
+  //Existen dos arrays. A partir de ello, escribir una función que permita 
+  //retornar un nuevo array con la intersección de ambos elementos. (Ej: [4,2,3] unión [1,7,4,3] = [3,4].
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+  nuevo = []
+  arreglo1.forEach(numero => {
+    if(arreglo2.includes(numero)){
+      nuevo.push(numero)
+    }
+  })
+  return nuevo
 }
 
 
